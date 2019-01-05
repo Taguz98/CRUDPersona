3ac65f4089dfea9da4b6ef4229ce0ac80ba8f546
Taguz98 <gus199811@gmail.com>
2019-01-04 19:35:43 -0500

    Termina la intefaz grafica, falta agregar animaciones de botones y la ventana

---
+++ src/controlador/CRUDPersona.java
@@ -0,0 +1,14 @@
+
+package controlador;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CRUDPersona {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/controlador/PersonaCTR.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import vista.PersonaUI;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PersonaCTR {
+    
+    private final PersonaUI vtnPersona;
+    
+    public PersonaCTR(PersonaUI vtnPersona){
+        this.vtnPersona = vtnPersona;
+        vtnPersona.setVisible(true); 
+    }
+    
+    public void iniciar(){ 
+        
+    }
+    
+    public void nuevaPersona(){
+        
+    }
+    
+    public void eliminarPersona(){ 
+        
+    }
+    
+    
+}
